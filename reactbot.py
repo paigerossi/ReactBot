48b4be724468a334beaa75dde42ede8e9e121694
Paige Rossi <paigerossi@yahoo.com>
2017-11-11 18:00:48 -0500

    some progress

--- reactbot.py
+++ reactbot.py
@@ -8,13 +8,21 @@ BOT_ID = os.environ.get("BOT_ID")
 #other variables
 AT_BOT = "<@" + BOT_ID + ">"
 Ex_Command = "print reactions"
+key = "volleyball"
 
 #instantiate Slack and Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
+def findMessage(channel, key):
+	allMessages = slack_client.api_call("channel.history",channel=channel)
+
+	for message in allMessages:
+		if "react to" and key in message["text"]:
+			return message["ts"]
 
 def handle_command(command, channel):
 	""" Receives commands directed at the bot"""
+
 	response = "Please begin your command with the phrase 'print reactions'"
 	if command.startswith(Ex_Command):
 		response = 'nice'
@@ -26,6 +34,7 @@ def handle_command(command, channel):
 def parse_slack_output(slack_rtm_output):
     """Returns None unless a message is directed at the Bot, 
     based on its ID."""
+
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
@@ -33,18 +42,24 @@ def parse_slack_output(slack_rtm_output):
                 # return text after the @ mention, whitespace removed
                 return output['text'].split(AT_BOT)[1].strip().lower(), \
                        output['channel']
+
+            #if statement for if output is not directed at bot but holds keywords
     return None, None
 
 
-if __name__ == "__main__":
-	READ_WEBSOCKET_DELAY = 1 
-	if slack_client.rtm_connect():
-		print("ReactBot connected!")
-		while True:
-			command, channel = parse_slack_output(slack_client.rtm_read())
-			if command and channel:
-				handle_command(command, channel)
-			time.sleep(READ_WEBSOCKET_DELAY)
+def main():
 
-	else:
-		print("Connection failed. Invalid Slack token or bot id?")
+	if __name__ == "__main__":
+		READ_WEBSOCKET_DELAY = 1 
+		if slack_client.rtm_connect():
+			print("ReactBot connected!")
+			while True:
+				command, channel = parse_slack_output(slack_client.rtm_read())
+				if command and channel:
+					handle_command(command, channel)
+				time.sleep(READ_WEBSOCKET_DELAY)
+        
+		else:
+			print("Connection failed. Invalid Slack token or bot id?")
+        id = findMessage(channel)
+main()
